b6a5388532e310401c80a5400f8f122b06685a0a
Andrey Postupalenko <andrey.postupalenko@saltpepper.ru>
2020-06-24 09:26:20 +0300

    Lesson 1

--- square_equation.rb
+++ square_equation.rb
@@ -8,10 +8,13 @@ print 'Enter third side (c): '
 c = gets.chomp.to_i
 
 d = b**2 - (4 * a * c)
-if d < 0
+if d > 0
   x1 = (-b + Math.sqrt(d.abs)) / (2.0 * a) 
   x2 = (-b - Math.sqrt(d.abs)) / (2.0 * a) 
   puts "D = #{d}, roots are x1 = #{x1.round(3)}, x2 = #{x2.round(3)}"
-else 
-  puts 'There is no root'
+elsif d == 0  
+	x = (-b + Math.sqrt(d.abs)) / (2.0 * a) 
+	puts "D = #{d}, root x1,2 = #{x.round(3)}"
+else
+  puts "D = #{d}, There is no root"
 end
